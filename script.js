44841a819b8e53ad498a9de3a4f89f5feea0e173
Hassan Muhayyudin <muhayyudinhassan@gmail.com>
2025-12-11 17:43:11 +0500

    Refactor color change to use hex format and add copy feature

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const btn = document.getElementById('chgBtn');
+const copyBtn = document.getElementById('copyBtn');
+
 btn.addEventListener("click", () => {
-    let r = Math.floor(Math.random() * 256)
-    let g = Math.floor(Math.random() * 256)
-    let b = Math.floor(Math.random() * 256)
-    let color = document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    btn.innerText = color;
-    btn.style.color = color;
+    let col = "#";
+    for (let i = 0; i < 6; i++) {
+        col += Math.floor(Math.random() * 16).toString(16);
+    }
+    document.body.style.backgroundColor = col;
+    btn.innerText = col;
+    btn.style.color = col;
 });
 
-// btn.addEventListener("click", () => {
-//     let col = "#";
-//     for (let i = 0; i < 6; i++) {
-//         col += Math.floor(Math.random() * 16).toString(16);
-//     }
-//     document.body.style.backgroundColor = col;
-//     btn.innerText = col;
-// });
+copyBtn.addEventListener("click", () => {
+    const color = btn.innerText;
+    navigator.clipboard.writeText(color).then(() => {
+        alert(`Copied the color: ${color}`);
+    }).catch(err => {
+        console.error('Failed to copy text: ', err);
+    });
+});
+    // btn.addEventListener("click", () => {
+    //     let r = Math.floor(Math.random() * 256)
+    //     let g = Math.floor(Math.random() * 256)
+    //     let b = Math.floor(Math.random() * 256)
+    //     let color = document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    //     btn.innerText = color;
+    //     btn.style.color = color;
+    // });
